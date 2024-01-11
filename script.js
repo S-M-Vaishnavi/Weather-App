373b3b2f96f665cf6a56c111513f132d3754b77b
S-M-Vaishnavi <smvaishnavi23@gmail.com>
2024-01-11 11:22:08 +0530

    updated

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const API_KEY = "315e60435b8a4d4ac2082f828014f3e0";
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q={city name}&appid={API_KEY}";
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + API_KEY;
 
     const searchBox = document.querySelector(".search input");
     const searchBtn = document.querySelector(".search button");
     const weatherIcon = document.querySelector(".weather-icon");
 
-    async function checkWeather(city) {
-        const response = await fetch(API_URL + city + `&aapid=${API_KEY}`);
-        
-        if(response.status == 404){
+    async function checkWeather() {
+        const city = searchBox.value;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+
+        if (response.status == 404) {
             document.querySelector(".error").style.display = "block";
             document.querySelector(".weather").style.display = "none";
-        }else{
+        } else {
             let data = await response.json();
             console.log(data);
             document.querySelector(".city").innerHTML = data.name;
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
             document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
             document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-    
+
             if (data.weather[0].main == "Clouds") {
                 weatherIcon.src = "images/clouds.png";
             } else if (data.weather[0].main == "Clear") {
@@ -31,15 +33,19 @@ document.addEventListener("DOMContentLoaded", () => {
             } else if (data.weather[0].main == "Mist") {
                 weatherIcon.src = "images/mist.png";
             }
-    
-            document.querySelector(".weather").computedStyleMap.display = "block";
-            document.querySelector(".error").style.display = "none";
 
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
         }
-       
     }
 
+    searchBox.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            checkWeather();
+        }
+    });
+
     searchBtn.addEventListener("click", () => {
-        checkWeather(searchBox.value);
+        checkWeather();
     });
 });
